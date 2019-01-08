376ae8dff78b29c6e87c6c24523dae639baec99e
Albert221 <w.albert221@gmail.com>
2019-01-08 21:13:17 +0100

    Show thumbnails in collections activity

--- app/src/main/kotlin/wolszon/me/shopifydeveloperchallenge/android/ui/collections/item/CollectionItemViewHolder.kt
+++ app/src/main/kotlin/wolszon/me/shopifydeveloperchallenge/android/ui/collections/item/CollectionItemViewHolder.kt
@@ -1,18 +1,17 @@
 package wolszon.me.shopifydeveloperchallenge.android.ui.collections.item
 
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
+import com.squareup.picasso.Picasso
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.collection_list_item.*
 import wolszon.me.shopifydeveloperchallenge.api.model.Collection
 
-class CollectionItemViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-    fun bindView(collection: Collection) {
+class CollectionItemViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
+    LayoutContainer {
+    fun bindView(collection: Collection, onClick: () -> Unit) {
         name.text = collection.title
-
-        container.setOnClickListener {
-            Log.d("TEST", "klik!")
-        }
+        Picasso.get().load(collection.imageSrc).into(thumbnail)
+        container.setOnClickListener { onClick() }
     }
 }
